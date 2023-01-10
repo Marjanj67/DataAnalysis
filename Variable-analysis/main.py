b9e9e2401efa0ed73d58db2bf0a525277da04c86
Marjan Jannati <117945484+Marjanj67@users.noreply.github.com>
2023-01-10 10:26:23 +0330

    Update main.py

--- Variable-analysis/main.py
+++ Variable-analysis/main.py
@@ -2,8 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import geopandas as gpd
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
 
 colors = sns.color_palette('Set2',10)
+colors1 = sns.color_palette('rocket',149)
 def clean_data(df):
     ListVariables = df.columns.values
     Unwanted = ['Standard error of ladder score', 'upperwhisker', 'lowerwhisker',
@@ -12,9 +16,38 @@ def clean_data(df):
                 'Explained by: Healthy life expectancy','Explained by: Freedom to make life choices',
                 'Explained by: Generosity',	'Explained by: Perceptions of corruption',	'Dystopia + residual']
     df.drop(columns = Unwanted,inplace = True)
-    Describe = df.describe()  # Everything is in order
-    Describe.to_csv('Describe.csv') 
+    # print(df.head(5))
+    # Describe = df.describe()  # Everything is in order
+    # Describe.to_csv('Describe.csv') 
     return df
+
+def plot_map(dfClean):
+    countries = gpd.read_file(
+               gpd.datasets.get_path("naturalearth_lowres"))
+    dfTemp = dfClean.loc[:,['Country name', 'Ladder score']]
+    dfTemp = dfTemp.sort_values('Ladder score')
+
+    # before merging name of some countries need to change
+    us = dfTemp[dfTemp['Country name']=='United States'].index.values[0]
+    dfTemp.at[us,'Country name'] = 'United States of America'
+    dr = dfTemp[dfTemp['Country name']=='Dominican Republic'].index.values[0]
+    dfTemp.at[dr,'Country name'] = 'Dominican Rep.'
+
+    MergedData = pd.merge(countries,dfTemp, how = "outer",left_on='name',right_on='Country name')
+    MergedData.loc[:,['Ladder score']] = MergedData.loc[:,['Ladder score']].fillna('2')
+    MergedData['Ladder score'] = pd.to_numeric(MergedData['Ladder score'])
+    fig, ax = plt.subplots(figsize=(8,6))
+    divider = make_axes_locatable(ax)
+
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+    MyMap = MergedData.plot(column = 'Ladder score' , cmap = 'Greens',ax = ax, legend=True, cax=cax)
+    ax.set_title('Map of happiness')
+    ax.text(-200,-130,'* Based on data from 2021')
+    ax.text(-200,-145,'* Data for the white countries is not available')
+    ax.text(-200,-160,'* Darker color means happier')
+    MyMap.set_facecolor('#ADD8E6')
+    fig.savefig('map.png')
+    # plt.show()
 
 def plot_scatter(dfClean):
     fig , ax = plt.subplots(2,3)
@@ -49,6 +82,7 @@ def plot_scatter(dfClean):
     ax[1,2].set_ylabel('Perceptions of corruption')
     fig.set_size_inches(15,10)
     fig.savefig('scatter.png')
+    # plt.show()
 
 def correlation_matrix(dfClean):
     CorrelationMatrix = dfClean.corr(method='pearson',numeric_only = True)
@@ -68,13 +102,15 @@ def plot_heatmap(dfClean):
         for j in range(len(Variables)):
             ax.text(i-.1,j,np.around(CovMatrix.iloc[i,j],decimals = 1))
     fig.savefig('heatmap.png')
+    # plt.show()
     
 def main ():
     data = pd.read_csv('world-happiness-report-2021.csv')
     df = pd.DataFrame(data)
     dfClean = clean_data(df)
-    dfClean.to_csv('dfClean.csv') 
+    # dfClean.to_csv('dfClean.csv') 
     plot_scatter(dfClean)
+    plot_map(dfClean)
     correlation_matrix(dfClean)
     plot_heatmap(dfClean)
 
